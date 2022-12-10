31d54de1510af9cea0befe000cc502b76fa487f9
Anurag <anuragchakravorty000@gmail.com>
2022-12-10 12:25:52 +0530

    check

---
+++ src/components/AsyncAwait.js
@@ -0,0 +1,11 @@
+//Writing the same code as ApiAxios but using async and Await.
+import React from 'react'
+const AsyncAwait = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AsyncAwait
